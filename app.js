00bd6d67fef59db052d9a4a6fdb2bb98b7209eb3
omarkamal62 <okamal34@gmail.com>
2025-03-17 16:30:44 +0200

    adding the user with bcrypt

--- app.js
+++ app.js
@@ -59,17 +59,6 @@ app.use(errorController.get404);
 mongoose
   .connect(MONGODB_URI)
   .then((result) => {
-    User.findOne().then((user) => {
-      if (!user) {
-        const user = new User({
-          name: "Omar",
-          email: "omar@mail.com",
-          cart: { items: [] },
-        });
-
-        user.save();
-      }
-    });
     app.listen(3001);
   })
   .catch((err) => console.log(err));
