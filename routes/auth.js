d308b3897e28a95fa61fc0a266b3dea40bf6077b
omarkamal62 <okamal34@gmail.com>
2025-03-18 17:00:22 +0200

    adding validation for login

--- routes/auth.js
+++ routes/auth.js
@@ -10,7 +10,16 @@ router.get("/login", authController.getLogin);
 
 router.get("/signup", authController.getSignup);
 
-router.post("/login", authController.postLogin);
+router.post(
+  "/login",
+  [
+    check("email").isEmail().withMessage("Please enter a valid email address"),
+    check("password", "Please enter a valid password")
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+  ],
+  authController.postLogin
+);
 
 router.post(
   "/signup",
