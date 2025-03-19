8fdc6a1d9d8ab60777197022ce0dfee4bb28e298
omarkamal62 <okamal34@gmail.com>
2025-03-19 15:02:37 +0200

    adding centralized error handling

--- controllers/admin.js
+++ controllers/admin.js
@@ -69,7 +69,10 @@ exports.postAddProduct = (req, res, next) => {
       //   validationErrors: [],
       // });
 
-      res.redirect("/500");
+      // res.redirect("/500");
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     });
 };
 
